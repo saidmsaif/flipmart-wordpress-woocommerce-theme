2f6da336887a1b7f06ba38a5462a2be0d7be1e26
saidmsaif <saidmsaiff@gmail.com>
2021-03-02 16:15:47 +0600

    Style Enqueue

--- functions.php
+++ functions.php
@@ -8,3 +8,17 @@
         add_theme_support( 'custom-logo' );
     }
     add_action('after_setup_theme', 'flipmart_theme_Setup');
+
+    function flipmart_assets_enqueue() {
+        // Stylesheet Enqueue
+        wp_enqueue_style( 'main-stylesheet', get_stylesheet_uri(  ) );
+        wp_enqueue_style( 'flipmart-main-stylesheet', get_template_directory_uri(  ).'/assets/css/main.css' );
+        wp_enqueue_style( 'flipmart-blue-stylesheet', get_template_directory_uri(  ).'/assets/css/blue.css' );
+        wp_enqueue_style( 'flipmart-owl-carousel-stylesheet', get_template_directory_uri(  ).'/assets/css/owl.carousel.css' );
+        wp_enqueue_style( 'flipmart-owl-transitions-stylesheet', get_template_directory_uri(  ).'/assets/css/owl.transitions.css' );
+        wp_enqueue_style( 'flipmart-animate-stylesheet', get_template_directory_uri(  ).'/assets/css/animate.min.css' );
+        wp_enqueue_style( 'flipmart-rateit-stylesheet', get_template_directory_uri(  ).'/assets/css/rateit.css' );
+        wp_enqueue_style( 'flipmart-bootstrap-select-stylesheet', get_template_directory_uri(  ).'/assets/css/bootstrap-select.min.css' );
+        wp_enqueue_style( 'flipmart-font-awesome-stylesheet', get_template_directory_uri(  ).'/assets/css/font-awesome.css' );
+    }
+    add_action( 'wp_enqueue_scripts', 'flipmart_assets_enqueue' );
